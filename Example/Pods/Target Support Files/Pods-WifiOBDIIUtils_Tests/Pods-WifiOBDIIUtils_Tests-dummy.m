36201986ab1fbf85c1b0607351d589fa4649c8a5
Lucas Bicca <lucaslbs15@gmail.com>
2017-11-22 20:22:38 -0200

    Project structure was created

---
+++ Example/Pods/Target Support Files/Pods-WifiOBDIIUtils_Tests/Pods-WifiOBDIIUtils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WifiOBDIIUtils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WifiOBDIIUtils_Tests
+@end
---
+++ Example/Pods/Target Support Files/WifiOBDIIUtils/WifiOBDIIUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WifiOBDIIUtils : NSObject
+@end
+@implementation PodsDummy_WifiOBDIIUtils
+@end
